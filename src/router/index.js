dbf7207e1f4b75dc3bcbf9c12821a1513f2a7bf7
Guilherme jeske <guilhermeieski@gmail.com>
2024-03-07 09:41:34 -0300

    cria ci para dev

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import HomeView from '../pages/home.vue';
 import LoginView from '../pages/login.vue';
 import RegisterView from '../pages/register.vue';
-import RedefinirView from '../pages/redefinir.vue';
+// import RedefinirView from '../pages/redefinir.vue';
 import authMiddleware from '@/middleware/authMiddleware'; // Importe o middleware
 import PrimeiroAcessoView from '../pages/primeiro-acesso'
 
@@ -10,7 +10,7 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 const routes = [
   { path: '/', component: HomeView, beforeEnter: authMiddleware, },
   { path: '/login', component: LoginView },
-  { path: '/redefinir', component: RedefinirView },
+  // { path: '/redefinir', component: RedefinirView },
   { path: '/register', component: RegisterView },
   { path: '/definir-senha', component: PrimeiroAcessoView },
 
